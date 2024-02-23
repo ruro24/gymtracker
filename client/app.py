ec4bc745d2e4ad432b1302b3bfa3190688af6875
ruro24 <rumukoro@hotmail.com>
2024-02-23 14:48:57 +0000

    edits to styling. developing backend

--- client/app.py
+++ client/app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 
 @app.route('/')
-def hello():
-    return 'Hello, Ru!'
+def home():
+    return render_template('home.html')
 
 
 if __name__ == "__main__":
---
+++ server/write_to_excel.py
@@ -0,0 +1,10 @@
+from openpyxl import Workbook
+
+
+def create_new_excel_file():
+    wb = Workbook
+    
+    return #location
+
+
+create_new_excel_file()
